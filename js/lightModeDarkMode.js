d6d942f727d1eb11cae6ba2198abfd63bb2e83bf
Luka Marjanovic <luka.marjanovic@medialake.ai>
2026-07-08 16:13:04 +0200

    fixed light mode dark mode button

--- js/lightModeDarkMode.js
+++ js/lightModeDarkMode.js
@@ -1,24 +1,29 @@
-if (typeof (Storage) !== "undefined") {
-  var modeVar = 1;
-  
-  function modeButton() {
-    if (modeVar == 1) {
-      darkMode();
-      modeVar = 0;
-    }
-    else {
-      lightMode();
-      modeVar = 1;
-    }
-  }
+var modeVar = 1;
+const filename = document.location.pathname.split("/").pop();
 
-  function darkMode() {
+function modeButton() {
+  if (modeVar == 1) {
+    darkMode();
+    modeVar = 0;
+  }
+  else {
+    lightMode();
+    modeVar = 1;
+  }
+}
+
+function darkMode() {
+  if (filename === "index.html" || filename === "") {
+    document.getElementById("style").href = "css/darkMode.css";
+  } else {
     document.getElementById("style").href = "../css/darkMode.css";
   }
+}
 
-  function lightMode() {
+function lightMode() {
+  if (filename === "index.html" || filename === "") {
+    document.getElementById("style").href = "css/lightMode.css";
+  } else {
     document.getElementById("style").href = "../css/lightMode.css";
   }
-} else {
-  alert('Sorry! No Web Storage support..');
 }
